57f86acf534c006e2172ae19c810f16318718d94
Binderiya <binderiyatsogthers@gmail.com>
2023-03-07 17:41:06 +0800

    product, category dutuu bgaa shuu hiigeerei

--- route/user-router.js
+++ route/user-router.js
@@ -11,52 +11,57 @@ const router = express.Router();
 
 // getAll
 router.get("/", async (req, res) => {
-  try {
-    res.json(await getUser());
-  } catch (error) {
-    res.status(400).json("Someting went wrong");
-  }
+  res.json(await getUser());
 });
 
 //getOne
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
-  try {
-    res.json(await getOneUser(id));
-  } catch (err) {
-    res.status(400).json("Someting went wrong");
-  }
+  res.json(await getOneUser(id));
 });
 
 //Post
 router.post("/", async (req, res) => {
-  const { name, email, number, address } = req.body;
-  try {
-    res.json(await createUser(name, email, number, address));
-  } catch (err) {
-    res.status(400).json("Someting went wrong");
-  }
+  const { firstName, lastName, birthDate, email, phone, password, imageUrl } =
+    req.body;
+
+  const user = await createUser({
+    firstName,
+    lastName,
+    birthDate,
+    email,
+    phone,
+    password,
+    imageUrl,
+  });
+
+  res.json(user);
 });
 
 //Delete
 router.delete("/:id", async (req, res) => {
   const { id } = req.params;
-  try {
-    res.json(await deleteUser(id));
-  } catch (err) {
-    res.status(400).json("Something went wrong");
-  }
+  res.json(await deleteUser(id));
 });
 
 //Patch
 router.patch("/:id", async (req, res) => {
   const { id } = req.params;
-  const { name, email, number, address } = req.body;
-  try {
-    res.json(updateUser(name, email, number, address, id));
-  } catch (error) {
-    res.status(400).json("Someting went wrong");
-  }
+  const { firstName, lastName, birthDate, email, phone, password, imageUrl } =
+    req.body;
+
+  res.json(
+    updateUser({
+      firstName,
+      lastName,
+      birthDate,
+      email,
+      phone,
+      password,
+      imageUrl,
+      id,
+    })
+  );
 });
 
 export default router;
--- services/user-service.js
+++ services/user-service.js
@@ -1,32 +1,72 @@
 import pool from "../config/mysql-config.js";
-
+import { nanoid } from "nanoid";
+//GET ALL USERS
 export const getUser = async () => {
-  const [result, columns] = await pool.query("SELECT * FROM user");
+  const [result] = await pool.query("SELECT * FROM user");
   return result;
 };
-
+//GET USER BY ID
 export const getOneUser = async (id) => {
-  const [[result]] = await pool.query(`SELECT * FROM user WHERE id=?`, [id]);
-  return result;
+  const [result] = await pool.query(`SELECT * FROM user WHERE userId=?`, [id]);
+  const user = result.length ? result[0] : null;
+  return user;
 };
 
-export const createUser = async (name, email, number, address) => {
-  const [result] = await pool.query(
-    `INSERT INTO user (name, email, number, address) VALUES (?,?,?,?)`,
-    [name, email, number, address]
-  );
-  return result;
+//POST
+export const createUser = async (user) => {
+  const id = nanoid();
+  try {
+    await pool.query(
+      `INSERT INTO user (userId, firstName, lastName, birthDate, email, phone, password, imageUrl, createdAt) VALUES (?,?,?,?,?,?,?,?,?)`,
+      [
+        id,
+        user.firstName,
+        user.lastName,
+        user.birthDate,
+        user.email,
+        user.phone,
+        user.password,
+        user.imageUrl,
+        new Date(),
+      ]
+    );
+    const result = await getOneUser(id);
+    return result;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 };
-
+//DELETE
 export const deleteUser = async (id) => {
-  const [result] = await pool.query(`DELETE FROM user WHERE id=?`, [id]);
-  return result;
+  try {
+    await pool.query(`DELETE FROM user WHERE id=?`, [id]);
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
 };
-
-export const updateUser = async (name, email, number, address) => {
-  const [result] = await pool.query(
-    `UPDATE user SET name=?,email=?, number=?, address=? WHERE id=?`,
-    [name, email, number, address, id]
-  );
-  return result;
+//UPDATE
+export const updateUser = async (user) => {
+  try {
+    await pool.query(
+      `UPDATE user SET firstName=?, lastName=?, birthDate=?, email=?, phone=?, password=?, imageUrl=? updatedAt=? where userId=?`,
+      [
+        user.firstName,
+        user.lastName,
+        user.birthDate,
+        user.email,
+        user.phone,
+        user.password,
+        user.imageUrl,
+        new Date(),
+        user.id,
+      ]
+    );
+    const result = await getUser(id);
+    return result;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
 };
